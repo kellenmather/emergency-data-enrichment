fded68b9b8a3cceb6de6bd2985b15af251cd6ee3
Kellen Preston <kellenmpreston@gmail.com>
2020-08-15 13:07:45 -0500

    map added

---
+++ src/components/Map.vue
@@ -0,0 +1,32 @@
+<template>
+  <gmapsMap>
+    <gmapsMarker :position="{ lat: -27, lng: 153 }" />
+  </gmapsMap>
+</template>
+
+<script>
+import { gmapsMap, gmapsMarker } from 'x5-gmaps'
+
+export default {
+  name: 'Map',
+  components: { gmapsMap, gmapsMarker }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import x5GMaps from 'x5-gmaps'
 
 Vue.config.productionTip = false
+Vue.use(x5GMaps, 'AIzaSyDy3qAZC4cLE5kGGJ_QNkenpi5cbDXHvng' )
 
 new Vue({
   render: h => h(App),
