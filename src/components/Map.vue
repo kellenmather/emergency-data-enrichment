98c575970d32c05bbc199210fa6eb22fd7d88b58
Kellen Preston <kellenmpreston@gmail.com>
2020-08-15 13:32:16 -0500

    added accident data and show location of accident on map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,15 +1,36 @@
 <template>
-  <gmapsMap>
-    <gmapsMarker :position="{ lat: -27, lng: 153 }" />
+  <gmapsMap
+    :options="mapOptions">
+    <gmapsMarker v-for="(accident, index) in populateAccidents()" :key="index" :position="{ lat: accident.address.latitude, lng: accident.address.longitude }" />
   </gmapsMap>
 </template>
 
 <script>
 import { gmapsMap, gmapsMarker } from 'x5-gmaps'
+import Records from '@/accidentRecords/records'
 
 export default {
   name: 'Map',
-  components: { gmapsMap, gmapsMarker }
+  components: { gmapsMap, gmapsMarker },
+  data: function () {
+    return {
+      mapOptions: {
+        center: { lat: 37.541885, lng: -77.440624 },
+        zoom: 14,
+        fullscreenControl: false,
+        mapTypeControl: false,
+        rotateControl: false,
+        scaleControl: false,
+        streetViewControl: false
+      },
+      accidents: null
+    }
+  },
+  methods: {
+    populateAccidents() {
+      return Records.record;
+    }
+  }
 }
 </script>
 
